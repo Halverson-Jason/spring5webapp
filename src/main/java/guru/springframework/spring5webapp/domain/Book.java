e5b61d2677075463f5c744aa406895107067f8c9
Jason Halverson <halv.jason@gmail.com>
2020-02-04 16:55:57 -0800

    Publisher JPA

--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -4,11 +4,16 @@ import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+
+
+
 @Entity
 public class Book {
+
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
+
     private String title;
     private String isbn;
 
@@ -17,6 +22,10 @@ public class Book {
             inverseJoinColumns = @JoinColumn(name = "author_id") )
     private Set<Author> authors = new HashSet<>();
 
+
+    @ManyToOne
+    private Publisher publisher;
+
     public Set<Author> getAuthors() {
         return authors;
     }
@@ -24,6 +33,14 @@ public class Book {
     public Book(String title, String isbn) {
         this.title = title;
         this.isbn = isbn;
+    }
+
+    public Publisher getPublisher() {
+        return publisher;
+    }
+
+    public void setPublisher(Publisher publisher) {
+        this.publisher = publisher;
     }
 
     public Long getId() {
